448e9f14b7f8b9f06ad763d33eb4dc910cf84de2
s-dasamandams <s-dasamandams@53S500038039.it.bsd405.org>
2019-11-13 16:48:48 -0800

    First Checkpoint

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,32 +1,48 @@
 /**
  * @author Mr. Rasmussen
  */
-
 package fracCalc;
 
+import java.util.*;
+
 public class FracCalc {
+	
+public static void main(String[] args) {
+		{
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter an expression with space separating operands and operator: (eg : 1_2/3 + 2_3/4)");
+			String input = sc.nextLine();
 
-    public static void main(String[] args)
-    {
-        // TODO: Read the input from the user and call produceAnswer with an equation
+			String result = produceAnswer(input);
+			System.out.println("Result = " + result);
+			sc.close();
+	}
+		// TODO: Read the input from the user and call produceAnswer with an equation
+	}
 
-    }
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) {
+		int firstSpace = input.indexOf(' ');
+		int lastSpace = input.lastIndexOf(' ');
 
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    {
-        // TODO: Implement this function to produce the solution to the input
+		String op1 = input.substring(0, firstSpace);
 
-        return "";
-    }
+		String op = input.substring(firstSpace + 1, lastSpace);
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+		String op2 = input.substring(lastSpace + 1, input.length());
 
+		return op2;
+	}
 }
+// TODO: Fill in the space below with any helper methods that you think you will
+// need
