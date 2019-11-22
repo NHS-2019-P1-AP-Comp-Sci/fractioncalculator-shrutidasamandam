875f1838da56d24dcddd74eb5ccec9b0570401e5
s-dasamandams <s-dasamandams@53S500038039.it.bsd405.org>
2019-11-21 19:28:07 -0800

    Checkpoint 2 completed :)

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -4,45 +4,66 @@
 package fracCalc;
 
 import java.util.*;
+import java.util.ArrayList;
+
+import net.sf.saxon.functions.Substring;
 
 public class FracCalc {
-	
-public static void main(String[] args) {
-		{
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter an expression with space separating operands and operator: (eg : 1_2/3 + 2_3/4)");
-			String input = sc.nextLine();
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter an expression with space separating operands and operator: (eg : 1_2/3 + 2_3/4)");
+		System.out.println("Type quit to end");
+		String input = sc.nextLine();
+		while (!input.equals("quit")) {
 			String result = produceAnswer(input);
 			System.out.println("Result = " + result);
-			sc.close();
-	}
-		// TODO: Read the input from the user and call produceAnswer with an equation
+			input = sc.nextLine();
+		}
+		
 	}
 
+	// TODO: Read the input from the user and call produceAnswer with an equation
 	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
 	// test your code
 	// This function takes a String 'input' and produces the result
-	//
-	// input is a fraction string that needs to be evaluated. For your program, this
-	// will be the user input.
 	// e.g. input ==> "1/2 + 3/4"
-	//
-	// The function should return the result of the fraction after it has been
-	// calculated
 	// e.g. return ==> "1_1/4"
 	public static String produceAnswer(String input) {
 		int firstSpace = input.indexOf(' ');
 		int lastSpace = input.lastIndexOf(' ');
-
-		String op1 = input.substring(0, firstSpace);
-
-		String op = input.substring(firstSpace + 1, lastSpace);
-
-		String op2 = input.substring(lastSpace + 1, input.length());
-
-		return op2;
-	}
+		String substr1 = input.substring(0, firstSpace);
+		String substr2 = input.substring(firstSpace + 1, lastSpace);
+		String substr3 = input.substring(lastSpace + 1, input.length());
+		//assigned names
+		String wholeNum;
+		String num;
+		String den;
+		int wholeNumIndex= substr3.indexOf("_");
+		int fracIndex = substr3.indexOf("/");
+		
+		if(wholeNumIndex!=-1) {
+			wholeNum= substr3.substring(0, wholeNumIndex);
+		}else if (fracIndex == -1) {
+			wholeNum=substr3;
+		}else {
+			wholeNum="0";
+		}
+		if (fracIndex !=-1) {
+			num = substr3.substring(wholeNumIndex + 1, fracIndex);
+			den = substr3.substring(fracIndex +1);
+			
+		}else {
+			num = "0";
+			den = "1";
+		}
+//		if(Integer.parseInt(num) >= Integer.parseInt(den)) {
+//			int num3 = Integer.parseInt(num)%Integer.parseInt(den);
+//			int num2 = Integer.parseInt(num);
+//			num2 /= Integer.parseInt(den);
+//			wholeNum = num2 +"";
+//			num = num3 + "";
+//		}
+		String substrthree = "whole:"+wholeNum+" numerator:"+num+" denominator:"+den;
+		return substrthree;
 }
-// TODO: Fill in the space below with any helper methods that you think you will
-// need
+}
